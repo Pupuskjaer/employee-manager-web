b23161aed17caa352d7f3f541d7b49706242c31a
KhasanovTR <khasanovtimur2001@gmail.com>
2024-02-18 20:07:45 +0400

    Работа с Коллекциями

---
+++ employee-manager-web/src/main/java/com/khasanovtr/employeemanagerweb/EmployeeManagerWebApplication.java
@@ -0,0 +1,13 @@
+package com.khasanovtr.employeemanagerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerWebApplication.class, args);
+	}
+
+}
